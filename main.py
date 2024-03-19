77cfd77f8f65557e37599c7888d04320d812f2c5
rjm <robjmass@gmail.com>
2024-03-18 21:00:30 -0400

    unnamed commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import hashlib
 import json
 import time
+import requests
+from flask import Flask, jsonify, request
+from urllib.parse import urlparse
 
 def hashGenerator(data):
     result = hashlib.sha256(data.encode())
@@ -28,6 +31,8 @@ class Blockchain:
         genesis = Block(0, time.time(), 'gen_data', hashFirst, hashLast)
         self.chain = [genesis]
 
+        self.nodes = set()
+
     def add_block(self, data):
         prev_hash = self.chain[-1].hash
         hash = hashGenerator(data+prev_hash)
@@ -44,11 +49,49 @@ class Blockchain:
                 return False
         return True
 
+    def register_node(self, address):
+        parsed_url = urlparse(address)
+        self.nodes.add(parsed_url.netloc)
 
-blch = Blockchain()
-blch.add_block('A')
-blch.add_block('B')
-blch.add_block('C')
+    def consensus(self):
+        neighbors = self.nodes
+        new_chain = None
+        max_length = len(self.chain)
 
-for block in blch.chain:
-    print(block.__dict__)
+        for node in neighbors:
+            response = requests.get(f'http://{node}/chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid():
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
+
+app = Flask(__name__)
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+    # Implement node registration logic here
+    return "Nodes registered successfully", 201
+
+@app.route('/nodes', methods=['GET'])
+def get_nodes():
+    # Implement logic to get the list of nodes
+    nodes = [...]  # Replace [...] with actual list of nodes
+    return jsonify(nodes), 200
+
+if __name__ == '__main__':
+    blch = Blockchain()
+    blch.add_block('A')
+    blch.add_block('B')
+    blch.add_block('C')
+    for block in blch.chain:
+        print(block.__dict__)
+    app.run(debug=True)
+
